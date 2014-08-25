8585b65c894f3fffee774747d48a753ffa796cf5
Michelly Menezes <martha.menezes@ccc.ufcg.edu.br>
2014-08-25 17:57:48 +1000

    mapa -> matriz

--- planpath/src/planpath/Map.java
+++ planpath/src/planpath/Map.java
@@ -6,32 +6,41 @@ import java.util.ArrayList;
 
 public class Map {
 
-	private ArrayList map;
+	ArrayList map;
+	private ArrayList info;
 	private BufferedReader input;
 
 	public Map() throws Exception {
 
 		this.map = new ArrayList();
+		this.info = new ArrayList();
 		input = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/src/planpath/input.txt"));
+		int cont = 0;
 		while (input.ready()) {
-			map.add(input.readLine().toUpperCase());
+			if(cont < 3){
+				info.add(input.readLine().toUpperCase());
+			}
+			else{
+				map.add(input.readLine().toUpperCase().split(" "));
+			}
+			cont++;
 		}
 		input.close();
 	}
 	
 	public ArrayList getMap(){
-		return (ArrayList) map.subList(3, map.size()-1);
+		return map;
 	}
 	
 	public String getAlg(){
-		return (String) map.get(0);
+		return (String) info.get(0);
 	}
 	
 	public int getInt(){
-		return Integer.parseInt((String) map.get(1));
+		return Integer.parseInt((String) info.get(1));
 	}
 	
 	public int getSize(){
-		return Integer.parseInt((String) map.get(2));
+		return Integer.parseInt((String) info.get(2));
 	}
 }
