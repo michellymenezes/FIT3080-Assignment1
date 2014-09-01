6bec3f64ae91e5f61e7993db3fbf1cc504a6c70c
Michelly Menezes <martha.menezes@ccc.ufcg.edu.br>
2014-09-01 16:14:11 +1000

    coomentarios no backtrack

--- planpath/src/planpath/backtrack.java
+++ planpath/src/planpath/backtrack.java
@@ -12,12 +12,16 @@ public class Backtrack {
 	public Backtrack(int size, ArrayList<String []> table) throws Exception {
 		
 		this.size = size;
-		
 		this.table = table;
 		this.states = new ArrayList<ArrayList<String>>();
 		this.visited = createVisited();
 
 	}
+	
+	/* 
+	 * This method will return the movements chosen from the S to the G.
+	 * First it will search for the tile S and, after that, applies the data in the recursive function.
+	 */
 
 	public ArrayList<String> backtrack() {
 		ArrayList<String> moves = new ArrayList<String>();
@@ -35,6 +39,16 @@ public class Backtrack {
 		return moves;
 	}
 
+	/*
+	 * This is the recursive method to find the G.
+	 * It will check if it is possible to move to another tile according to the specification and add the move to a list for each movement.
+	 * If in a specific case it finds the G, it will return all moves until the current state.
+	 * If not, will apply the current state in a recursive call.
+	 * In each case it update a boolean copy  of the map to not return to a previous tile.
+	 * At the end of each case, it will remove the movement from the list, so the idea of backtrack can be done.
+	 * If it can not enter in none of the cases, it returns null.
+	 */
+	
 	private ArrayList<String> backtrackRecursive(int i, int j, ArrayList<String> moves, ArrayList<boolean []> visited) {
 		
 		states.add(possibleMoves(i, j));
@@ -172,6 +186,10 @@ public class Backtrack {
 		return null;
 	}
 	
+	/*
+	 * This method creates version of the map filled with boolean objects and return it.
+	 * This Array List will be used to not return to a visited tile.
+	 */
 	private ArrayList<boolean []> createVisited(){
 		ArrayList<boolean []> visitedTable = new ArrayList<boolean []>();		
 		for(int i = 0; i < size; i++){
@@ -189,6 +207,9 @@ public class Backtrack {
 		return visitedTable;
 	}
 	
+	/*
+	 * This method verifies and return an Array List with all the possible moves in a specific state
+	 */
 	private ArrayList<String> possibleMoves(int i, int j){
 		ArrayList<String> possibleMoves = new ArrayList<String>();
 		goR(i, j, possibleMoves);
@@ -202,6 +223,12 @@ public class Backtrack {
 		
 		return possibleMoves;
 	}
+	
+	/*
+	 * All the methods below verifies if a specific movement is possible.
+	 * If it is possible, the letter of the movement will be added in a list and true will be returned.
+	 * If not, it will return false.
+	 */
 	
 	private boolean goR(int i, int j , ArrayList<String> possibleMoves){
 		if(j < size - 1 && !table.get(i)[j + 1].equals("X") && !visited.get(i)[j + 1]){
